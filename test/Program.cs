a0edd291c594ec925e0f7b29240999c2f3a2712c
messagesusan <messagesusan@163.com>
2016-06-18 14:42:19 +0800

    同步代码

--- test/Program.cs
+++ test/Program.cs
@@ -10,7 +10,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("one,this is my fist program!hahha1");
+            Console.WriteLine("one,this is my fist wokao,haimeihaoa");
         }
     }
 }
